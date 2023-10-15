64550afa0b649aa2b0f835e3e27f9929b9bbc969
Dhayu Intan Nareswari <intaandh@gmail.com>
2023-10-15 23:29:59 +0700

    week-07

---
+++ week-07/basic_layout_flutter/lib/spacer.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        BlueBox(),
+        Spacer(flex: 1),
+        BlueBox(),
+        Spacer(flex: 1),
+        BlueBox(),
+      ],
+    );
+  }
+}
+
+class BlueBox extends StatelessWidget {
+  const BlueBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        border: Border.all(),
+      ),
+    );
+  }
+}
